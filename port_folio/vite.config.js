01b2638ffe7b8b3912e8b392706d8e63c09b31fa
R-abe <ryomar19xioathos@gmail.com>
2025-06-19 12:01:21 +0900

    README created

--- port_folio/vite.config.js
+++ port_folio/vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/portfolio/',  // 例: '/port_folio/'
+  base: '/ryoma1911.github.io/',  // 例: '/port_folio/'
   plugins: [react()],
 })
